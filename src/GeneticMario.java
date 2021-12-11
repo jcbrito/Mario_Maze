c8b5e23dd9aee2fa82ae103804e83ce97546ddc4
jbrito <jcbritov2@gmail.com>
2021-12-11 02:24:14 -0500

    added moving to the Mario

--- src/GeneticMario.java
+++ src/GeneticMario.java
@@ -5,6 +5,36 @@ public class GeneticMario {
         Mario mario = new Mario();
 
         System.out.println("Manhattan distance is: " + mario.score);
+
+        System.out.println("Original Board");
+        System.out.println("Curr location: (" + mario.currLocation[0] + ", " + mario.currLocation[1]+")");
+
+        mario.printMaze();
+        System.out.println();
+        System.out.println("Moves");
+        System.out.println("Curr location: (" + mario.currLocation[0] + ", " + mario.currLocation[1]+")");
+
+        mario.moveUp();
+        System.out.println("Curr location: (" + mario.currLocation[0] + ", " + mario.currLocation[1]+")");
+
+        mario.moveUp();
+        System.out.println("Curr location: (" + mario.currLocation[0] + ", " + mario.currLocation[1]+")");
+
+        mario.printMaze();
+        System.out.println("Manhattan distance is: " + mario.score);
+
+//        System.out.println();
+//        System.out.println("Moved Down");
+//        mario.moveDown();
+//        mario.printMaze();
+//        System.out.println();
+//        System.out.println("Move right");
+//        mario.moveRight();
+//        mario.printMaze();
+//        System.out.println();
+//        System.out.println("Move Left");
+//        mario.moveLeft();
+//        mario.printMaze();
     }
 
 }
--- src/Mario.java
+++ src/Mario.java
@@ -2,8 +2,8 @@ public class Mario {
 
     int[][] board = {{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
             {1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 4, 0, 1},
-            {2, 0, 0, 0, 0, 4, 0, 0, 1, 1, 1, 0, 0, 0, 1},
-            {1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1},
+            {0, 0, 0, 0, 0, 4, 0, 0, 1, 1, 1, 0, 0, 0, 1},
+            {1, 0, 2, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1},
             {1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1},
             {1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1},
             {1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1},
@@ -14,17 +14,82 @@ public class Mario {
     int score;
     boolean dead = false;
 
+    int[] currLocation = findMario();
+
     public Mario(){
         evaluate();
     }
 
+    public void moves(int[] moveSet){
+
+
+        for (int i = 0; i < moveSet.length; i++){
+            if (moveSet[i] == 1)
+                System.out.println(""); //place holder Lol
+        }
+
+    }
+
+    public void moveUp(){
+
+
+        if (currLocation[0] != 0) {
+            if (board[currLocation[0] - 1][currLocation[1]] == 1) return;
+            if (board[currLocation[0] - 1][currLocation[1]] == 4) dead = true;
+
+            board[currLocation[0]][currLocation[1]] = 0;
+            board[currLocation[0] - 1][currLocation[1]] = 2;
+        }
+        currLocation[0]--;
+        evaluate();
+    }
+
+    public void moveDown(){
+
+        if (currLocation[0] != board.length ) {
+            if (board[currLocation[0] + 1][currLocation[1]] == 1) return;
+            if (board[currLocation[0] + 1][currLocation[1]] == 4) dead = true;
+
+            board[currLocation[0]][currLocation[1]] = 0;
+            board[currLocation[0] + 1][currLocation[1]] = 2;
+        }
+        currLocation[0]++;
+        evaluate();
+    }
+
+    public void moveRight(){
+
+        if (currLocation[1] != board[currLocation[0]].length - 1) {
+            if (board[currLocation[0]][currLocation[1] + 1] == 1) return;
+            if (board[currLocation[0]][currLocation[1] + 1] == 4) dead = true;
+
+            board[currLocation[0]][currLocation[1]] = 0;
+            board[currLocation[0]][currLocation[1] + 1] = 2;
+        }
+        currLocation[1]++;
+        evaluate();
+    }
+
+    public void moveLeft(){
+
+        if (currLocation[1] != 0 ) {
+            if (board[currLocation[0]][currLocation[1] - 1] == 1) return;
+            if (board[currLocation[0]][currLocation[1] - 1] == 4) dead = true;
+
+            board[currLocation[0]][currLocation[1]] = 0;
+            board[currLocation[0]][currLocation[1] - 1] = 2;
+        }
+        currLocation[1]--;
+        evaluate();
+    }
+
+
     public void evaluate(){
-        int[] mario = findMario();
         int[] goal = findGoal();
 
         score = 0;
-        score += Math.abs(mario[0] - goal[0]);
-        score += Math.abs(mario[1] - goal[1]);
+        score += Math.abs(currLocation[0] - goal[0]);
+        score += Math.abs(currLocation[1] - goal[1]);
     }
 
     private int[] findMario(){
@@ -47,5 +112,13 @@ public class Mario {
         return new int[] {0,0};
     }
 
+    public void printMaze(){
+        for (int i = 0; i < board.length; i++){
+            for (int j = 0; j < board[i].length; j++){
+                System.out.print(board[i][j] +" ");
+            }
+            System.out.println();
+        }
+    }
 
 }
